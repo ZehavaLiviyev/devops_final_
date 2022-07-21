7b8c6b40bb41e4f7e336b6b5edd0a3347f4d22d7
Eden-Kaduri <79143656+Eden-Kaduri@users.noreply.github.com>
2022-07-21 12:52:37 +0300

    Update for test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1> Hello World </h1>
+        <h1> Hello Eli </h1>
         <h2> Thanks for visiting! </h2>
         <h2> This is the final project for the most fun course in the Degree :) </h2>
     </div>
